875bb5806a2758a029ef1d8b04e4d433f6fb5203
Venkata Subhash <venkata.subhash@scripbox.com>
2020-08-20 12:01:09 +0530

    Modify home page for privint project

--- app/javascript/components/HomePage.js
+++ app/javascript/components/HomePage.js
@@ -7,13 +7,34 @@ class HomePage extends React.Component {
 
   render () {
     return (
-      <button
-        type='submit'
-        className='btn btn-success'
-        onClick={this.handleSubmit}
-      >
-        Submit
-      </button>
+      <div className="container-fluid">
+        <div className="mt-5">
+          <div className="mx-auto" style={{ "width": "585px" }}>
+            <h1> PrivInt Eye for patient safety </h1>
+            <hr style={{ "border-top": "1px solid #f8f9fa" }}/>
+          </div>
+          <div className="row mt-5 pl-5">
+            <div className="col-sm">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Please enter your free text here"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </div>
+            <div className="col-sm">
+              <button
+                type='submit'
+                className='btn btn-success'
+                onClick={this.handleSubmit}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
