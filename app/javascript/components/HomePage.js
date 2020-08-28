3233b243fe0cc5187c87f739f77b131253898023
Venkata Subhash <venkata.subhash@scripbox.com>
2020-08-28 22:45:31 +0530

    Add a probability button to show probability

--- app/javascript/components/HomePage.js
+++ app/javascript/components/HomePage.js
@@ -26,11 +26,15 @@ class HomePage extends React.Component {
     super();
     this.state = {
       inputText: "",
-      apiResponse: ""
+      apiResponse: "",
+      probabilityText: ""
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.showResponse = this.showResponse.bind(this);
+    this.renderProbabilityButton = this.renderProbabilityButton.bind(this);
+    this.setProbabilityText = this.setProbabilityText.bind(this);
     this.onSuccess = this.onSuccess.bind(this);
     this.onFailure = this.onFailure.bind(this);
   };
@@ -93,13 +97,48 @@ class HomePage extends React.Component {
     this.setState({ apiResponse: 'Fetching response failed' });
   }
 
+  renderProbabilityButton() {
+    return (
+      <button
+        type='submit'
+        className='btn btn-success'
+        onClick={this.setProbabilityText}
+      >
+        Show Probability
+      </button>
+    );
+  }
+
+  setProbabilityText() {
+    this.setState({ probabilityText: "Probability of patient falling down from bed is 48%. Changes in medication might help in getting sound sleep. Please integrate with patients EMR for better suggestions." });
+  }
+
+  showResponse() {
+    return (
+      this.state.apiResponse == "" ? (<div></div>) : (
+        <div>
+          <div className="row mt-5 pl-5">
+            <div className="col-sm">
+              <legend>{this.state.apiResponse}</legend>
+            </div>
+          </div>
+          <div className="row mt-5 pl-5">
+            <div className="col-sm">
+              {this.state.probabilityText == "" ? (this.renderProbabilityButton()) : (<legend>{this.state.probabilityText}</legend>)}
+            </div>
+          </div>
+        </div>
+      )
+    );
+  }
+
   render () {
     return (
       <div className="container-fluid">
         <div className="mt-5">
           <div className="mx-auto" style={{ "width": "585px" }}>
             <h1> PrivInt Eye for patient safety </h1>
-            <hr style={{ "border-top": "1px solid #f8f9fa" }}/>
+            <hr style={{ "borderTop": "1px solid #f8f9fa" }}/>
           </div>
           <div className="row mt-5 pl-5">
             <div className="col-sm">
@@ -122,11 +161,7 @@ class HomePage extends React.Component {
               </button>
             </div>
           </div>
-          {this.state.apiResponse == "" ? (<div></div>) : (<div className="row mt-5 pl-5">
-            <div className="col-sm">
-              <legend>{this.state.apiResponse}</legend>
-            </div>
-          </div>)}
+          {this.showResponse()}
         </div>
       </div>
     );
